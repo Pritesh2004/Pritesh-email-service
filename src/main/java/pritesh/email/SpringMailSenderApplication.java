e055205ebdfa1a352532706893ec84c7412a2dde
Pritesh2004 <pmpatilpritesh123@gmail.com>
2023-12-31 18:59:42 +0530

    first commit

---
+++ src/main/java/pritesh/email/SpringMailSenderApplication.java
@@ -0,0 +1,13 @@
+package pritesh.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pritesh/email/SpringMailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package pritesh.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
